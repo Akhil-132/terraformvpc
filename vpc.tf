65477d8cec2c55c1480a62664fe79d23f9775340
Akhilreddymalladi <105489408+Akhil-132@users.noreply.github.com>
2023-08-29 15:47:23 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "Customvpc" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "aws_internet_gateway" "mygateway" {
+  vpc_id = aws_vpc.Customvpc.id
+
+  tags = {
+    Name = "IGW"
+  }
+}
+resource "aws_route_table" "myroutetable" {
+  vpc_id = aws_vpc.Customvpc.id
+
+  route {
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mygateway.id
+  }
+}
+resource "aws_subnet" "mysubt" {
+  vpc_id     = aws_vpc.Customvpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "Main"
+  }
+ }
+  resource "aws_subnet_association" "subnet_a_association" {
+  subnet_id      = aws_subnet.mysubt.id
+  route_table_id = aws_route_table.myroutetable.id
+}
+}  
